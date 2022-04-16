91ed932982c0f8af9b71ab31516e37a6b4fc0a08
dzularsi <dzularsi24@gmail.com>
2022-04-16 09:40:08 +0700

    menambahkan Matakuliah.php,ModelBuku.php,ModelUser.php

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -23,15 +23,22 @@
                     </td>
                 </tr>
                 <tr>
+                    <td>Nama MTK</td>
+                    <td>:</td>
+                    <td>
+                        <?= $nama; ?>
+                    </td>
+                </tr>
+                <tr>
                     <td>SKS</td>
                     <td>:</td>
                     <td>
                         <?= $sks; ?>
                     </td>
                 </tr>
-                <tr>
-                    <td colspan="3" align="center"><a href="<?= base_url('matakuliah'); ?>">Kembali</a></td>
-                </tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('matakuliah');?>">Kembali</a>
+                </td>
             </table>
         </center>
     </body>
